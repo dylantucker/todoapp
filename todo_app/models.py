db77aea08a7f6a072e7f7934b2932ef60669f008
Dylan Tucker <dylan.tucker12@gmail.com>
2022-08-20 13:14:37 -0400

    created duedate functions

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your models here.
+def one_day():
+    return timezone.now() + timezone.timedelta(days=1)
+
+def one_week():
+    return timezone.now() + timezone.timedelta(days=7)
+
+def one_month():
+    return timezone.now() + timezone.timedelta(days=30)
