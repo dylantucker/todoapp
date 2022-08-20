71ff08f8d2a08b8beca361f8514ff3da6fe56fea
Dylan Tucker <dylan.tucker12@gmail.com>
2022-08-20 13:38:55 -0400

    added User and Task classes

--- todo_app/models.py
+++ todo_app/models.py
@@ -10,3 +10,33 @@ def one_week():
 
 def one_month():
     return timezone.now() + timezone.timedelta(days=30)
+
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True, null=False)
+    f_name = models.CharField(max_length=30, null=False)
+    l_name = models.CharField(max_length=30, null=False)
+    active_state = models.BooleanField(default=True)
+
+    def get_abs_url(self):
+        return reverse('list', args=[self.id])
+
+    def __str__(self):
+        return f'{self.f_name} {self.l_name}'
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=False, Blank=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    due_on = models.DateTimeField(default=one_day)
+    assigned_to = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def get_abs_url(self):
+        return reverse(
+            'item-update', args=[str(self.assigned_to.id), str(self.id)]
+        )
+    
+    def __str__(self):
+        return f'{self.title} due by {self.due_on}'
+
+    class Meta:
+        ordering = ["due_on"]
