523fd86044e759f233e742c8010d94de37777042
Dylan Tucker <dylan.tucker12@gmail.com>
2022-08-30 04:51:15 -0400

    added tests, basic html boilerplates to facilitate testing

--- todo_app/tests.py
+++ todo_app/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+def test_index_page():
+    url = reverse('index')
+    assert url == '/'
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
-
+from django.views.generic import ListView
+from .models import User
 # Create your views here.
+
+class ListListView(ListView):
+    model = User
+    template_name = 'todo_app/index.html'
