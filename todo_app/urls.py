525d7a2763ddbe4a4235715f149b76d25c08d578
Dylan Tucker <dylan.tucker12@gmail.com>
2022-08-31 13:39:57 -0400

    added task class

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ListListView.as_view(), name='index'),
+    path("", views.UserListView.as_view(), name='index'),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,19 @@
 from django.views.generic import ListView
-from .models import User
+from .models import User, Task
 # Create your views here.
 
-class ListListView(ListView):
+class UserListView(ListView):
     model = User
     template_name = 'todo_app/index.html'
+
+class TaskListView(ListView):
+    model = Task
+    template_name = 'todo_app/todo_task.html'
+
+    def get_queryset(self):
+        return Task.objects.filter(user_id=self.kwargs['task_id'])
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context['user'] = User.objects.get(id=self.kwargs['task_id'])
+        return context
